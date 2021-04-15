8a6c2453907fa57c4f614ec52bc5c1518a4aca66
DhanaLakshmi1779659 <81734112+DhanaLakshmi1779659@users.noreply.github.com>
2021-04-15 12:22:42 +0530

    Update example.java

--- example.java
+++ example.java
@@ -3,7 +3,7 @@ public class example
   public static void main(String Args[])
   {
      System.out.println("this is example build in jenkins");
-     System.out.println("this is update");
+     
     
   }
 }
